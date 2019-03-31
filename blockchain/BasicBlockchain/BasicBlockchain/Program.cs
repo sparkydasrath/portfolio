e6c80ce05a7e058c1597c8cfdf94762d3aa72660
Sparky Dasrath <sdasrath@gmail.com>
2019-03-31 16:23:17 -0400

    Scafolded basic project

---
+++ blockchain/BasicBlockchain/BasicBlockchain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicBlockchain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
